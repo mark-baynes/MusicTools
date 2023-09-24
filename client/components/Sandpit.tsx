21fc0f3743f4c2bc09287078b096081ab180b9e4
Mark Baynes <mark@jazzpiano.co.nz>
2023-09-25 09:27:17 +1300

    simple counter in sandpit

--- client/components/Sandpit.tsx
+++ client/components/Sandpit.tsx
@@ -2,6 +2,11 @@ import { useState, useEffect } from 'react'
 
 const Sandpit = () => {
   const [showSandpit, setShowSandpit] = useState(false) // State variable for the toggle
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+  document.title = `You've clicked ${count} times `
+})
 
   return (
     <div>
@@ -9,10 +14,14 @@ const Sandpit = () => {
         {showSandpit ? 'Hide Sandpit' : 'Show Sandpit'}
       </button>
 
-      {showSandpit && ( // Conditional rendering
+      {showSandpit && ( // Conditional rendering && means
         <div className="container">
           <h2>Sandpit</h2>
-          {/* Rest of your MusicApi specific code */}
+          <div>
+            <p>You've clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>Click</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+          </div>
         </div>
       )}
     </div>
